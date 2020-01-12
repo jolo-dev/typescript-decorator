92a6dd0863e50b37f5b5973748aeb744b4a73ec8
John Long <johnyscrazy@gmail.com>
2020-01-12 22:06:35 +0100

    Add more code

--- app.ts
+++ app.ts
@@ -1,70 +1,11 @@
-import APIServer from './APIServer';
-import { Request, Response } from "express";
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
+import APIServer from "./APIServer";
+import Person from './entities/Person';
 
-const server = new APIServer();
+export const apiServer = new APIServer();
+export const db = new JsonDB(new Config("entityDatabase", true, true, '/'));
 
-class APIRoutes {
+apiServer.addEntity<Person>(Person);
 
-    @logRoute()
-    @route("get", "/")
-    public indexRoute(req: Request, res: Response) {
-        return {
-            "Hello": "World"
-        };
-    }
-
-    @logRoute()
-    @route("get", "/people")
-    @authenticate("123456")
-    public peopleRoute(req: Request, res: Response) {
-        return {
-            people: [
-                {
-                    "firstName": "David",
-                    "lastName": "Tucker"
-                },
-                {
-                    "firstName:": "Sammy",
-                    "lastName": "Davis"
-                }
-            ]
-        };
-    }
-
-}
-
-function route(method: string, path: string): MethodDecorator {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        server.app[method](path, (req: Request, res: Response) => {
-            res.status(200).json(descriptor.value(req, res));
-        });
-    }
-}
-
-function logRoute(): MethodDecorator {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        const original = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-            let req = args[0] as Request;
-            console.log(`${req.url} ${req.method} Called`);
-            return original.apply(this, args);
-        }
-    };
-}
-
-function authenticate(key: string): MethodDecorator {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        const original = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-            const req = args[0] as Request;
-            const res = args[1] as Response;
-            const headers = req.headers;
-            if (headers.hasOwnProperty('apikey') && headers.apikey == key) {
-                return original.apply(this, args);
-            }
-            res.status(403).json({ error: "Not Authorized" });
-        }
-    }
-}
-
-server.start();
+apiServer.start();
